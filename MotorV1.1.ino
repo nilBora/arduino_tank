4667feae2f74c9528c25379d3a42e1a7869b2fd4
nilBora <brdnlsrg@gmail.com>
2019-04-01 22:22:29 +0300

    first commit

---
+++ MotorV1.1.ino
@@ -0,0 +1,44 @@
+// Тестировалось на Arduino IDE 1.0.5
+int IN1 = 2; // Input3 подключен к выводу 5 
+int IN2 = 3;
+int IN3 = 5; // Input3 подключен к выводу 5 
+int IN4 = 6;
+
+int ENA = 9;
+int ENB = 10;
+
+int LED_RADIO_A = 13;
+
+void setup()
+{
+  pinMode (IN4, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN1, OUTPUT);
+
+  //pinMode (ENA, OUTPUT);
+  //pinMode (ENB, OUTPUT);
+  
+  pinMode(LED_RADIO_A, INPUT);
+ // analogWrite(ENA, 255); 
+ // analogWrite(ENB, 255);
+}
+void loop()
+{
+    
+  if (digitalRead(LED_RADIO_A)) {
+
+    digitalWrite (IN2, HIGH); 
+    digitalWrite (IN1, LOW);  
+    
+    digitalWrite (IN4, LOW);
+    digitalWrite (IN3, HIGH); 
+    
+  } else {
+     digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN4, LOW);
+    digitalWrite(IN3, LOW);
+  }
+ 
+}
