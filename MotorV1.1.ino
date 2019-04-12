c293c2d3c5b56449e4373513c4335e19fc208492
nilBora <brdnlsrg@gmail.com>
2019-04-12 23:13:26 +0300

    bluethooth develop

--- MotorV1.1.ino
+++ MotorV1.1.ino
@@ -1,4 +1,4 @@
-// Тестировалось на Arduino IDE 1.0.5
+
 int IN1 = 2; // Input3 подключен к выводу 5 
 int IN2 = 3;
 int IN3 = 5; // Input3 подключен к выводу 5 
@@ -7,57 +7,79 @@ int IN4 = 6;
 int ENA = 9;
 int ENB = 10;
 
-int LED_RADIO_A = 13;
-int LED_RADIO_B = 12;
-int LED_RADIO_C = 8;
-int LED_RADIO_D = 7;
-
 void setup()
 {
-  pinMode (IN4, OUTPUT);
-  pinMode (IN3, OUTPUT);
-  pinMode (IN2, OUTPUT);
-  pinMode (IN1, OUTPUT);
+  Serial.begin(9600);
+   pinMode (IN4, OUTPUT);
+   pinMode (IN3, OUTPUT);
+   pinMode (IN2, OUTPUT);
+   pinMode (IN1, OUTPUT);
 
-  //pinMode (ENA, OUTPUT);
-  //pinMode (ENB, OUTPUT);
-  
-  pinMode(LED_RADIO_A, INPUT);
- // analogWrite(ENA, 255); 
- // analogWrite(ENB, 255);
+   digitalWrite(IN1, LOW);
+   digitalWrite(IN2, LOW);
+   digitalWrite(IN4, LOW);
+   digitalWrite(IN3, LOW);
 }
 void loop()
-{ 
-  if (digitalRead(LED_RADIO_A)) {
+{
+  if (Serial.available()) {
+    char c = Serial.read();
+    Serial.println(c);
+    switch(c) {
+      case '0': stopTank();
+        break;
+       case '1': upTank();
+        break;
+       case '2': downTank();
+        break;
+       case '3': leftTank();
+        break;
+       case '4': rightTank();
+        break;
+    }
+  }
+}
+
+void stopTank()
+{
+   digitalWrite(IN1, LOW);
+   digitalWrite(IN2, LOW);
+   digitalWrite(IN4, LOW);
+   digitalWrite(IN3, LOW);
+}
+
+void upTank()
+{
     digitalWrite (IN2, HIGH); 
-    digitalWrite (IN1, LOW);  
-    
-    digitalWrite (IN4, LOW);
-    digitalWrite (IN3, HIGH); 
-    
-  } else if (digitalRead(LED_RADIO_B)) {
-    digitalWrite (IN2, HIGH); 
-    digitalWrite (IN1, LOW);  
-    
-    digitalWrite (IN4, LOW);
-    digitalWrite (IN3, LOW);
-  } else if (digitalRead(LED_RADIO_C)) {
-    digitalWrite (IN2, LOW); 
     digitalWrite (IN1, LOW);  
     
     digitalWrite (IN4, LOW);
     digitalWrite (IN3, HIGH);
-  } else if (digitalRead(LED_RADIO_D)) {
-     digitalWrite (IN2, LOW); 
-     digitalWrite (IN1, HIGH);  
+}
+
+void downTank()
+{
+   digitalWrite (IN2, LOW); 
+   digitalWrite (IN1, HIGH);  
      
-     digitalWrite (IN4, HIGH);
-     digitalWrite (IN3, LOW); 
-  } else {
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN4, LOW);
-    digitalWrite(IN3, LOW);
-  }
- 
+   digitalWrite (IN4, HIGH);
+   digitalWrite (IN3, LOW); 
+}
+
+void leftTank()
+{
+   digitalWrite (IN2, LOW); 
+   digitalWrite (IN1, LOW);  
+    
+    digitalWrite (IN4, LOW);
+    digitalWrite (IN3, HIGH);
+}
+
+void rightTank()
+{
+   digitalWrite (IN2, HIGH); 
+   digitalWrite (IN1, LOW);  
+    
+   digitalWrite (IN4, LOW);
+   digitalWrite (IN3, LOW);
 }
